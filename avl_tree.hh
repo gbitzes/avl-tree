17e5afa302cc03f6c5174326aaaa33fed758110d
Georgios Bitzes <georgios.bitzes@cern.ch>
2019-05-13 19:47:29 +0200

    Add project skeleton and set up unit tests

---
+++ avl_tree.hh
@@ -0,0 +1,28 @@
+// ----------------------------------------------------------------------
+// File: avl_tree.hh
+// Author: Georgios Bitzes
+// ----------------------------------------------------------------------
+
+/************************************************************************
+ * avl_tree - a C++ implementation of an AVL tree with                  *
+ * std::map interface                                                   *
+ * Copyright (C)                                                        *
+ *                                                                      *
+ * This program is free software: you can redistribute it and/or modify *
+ * it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation, either version 3 of the License, or    *
+ * (at your option) any later version.                                  *
+ *                                                                      *
+ * This program is distributed in the hope that it will be useful,      *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ * GNU General Public License for more details.                         *
+ *                                                                      *
+ * You should have received a copy of the GNU General Public License    *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.*
+ ************************************************************************/
+
+template<typename T>
+class avl_tree {
+
+};
---
+++ test/unit.cc
@@ -0,0 +1,31 @@
+// ----------------------------------------------------------------------
+// File: unit.cc
+// Author: Georgios Bitzes
+// ----------------------------------------------------------------------
+
+/************************************************************************
+ * avl_tree - a C++ implementation of an AVL tree with                  *
+ * std::map interface                                                   *
+ * Copyright (C)                                                        *
+ *                                                                      *
+ * This program is free software: you can redistribute it and/or modify *
+ * it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation, either version 3 of the License, or    *
+ * (at your option) any later version.                                  *
+ *                                                                      *
+ * This program is distributed in the hope that it will be useful,      *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ * GNU General Public License for more details.                         *
+ *                                                                      *
+ * You should have received a copy of the GNU General Public License    *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.*
+ ************************************************************************/
+
+#include "avl_tree.hh"
+#include <gtest/gtest.h>
+
+TEST(AvlTree, BasicSanity) {
+  avl_tree<int> tree;
+  std::cout << &tree << std::endl;
+}
